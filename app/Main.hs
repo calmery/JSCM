ee33dd24871df5d5b2dac3978c50d14edeeca487
Marei Kikukawa <contact@calmery.me>
2018-12-29 16:44:22 +0900

    引数として受け取ったファイルをパースするように変更する

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,20 @@
 module Main where
 
-import           Parser    (parse)
-import           RIO
-import           System.IO
+import           Parser             (parse)
+import           RIO                hiding (hClose)
+import           RIO.List.Partial   (head)
+import           System.Environment (getArgs)
+import           System.IO          (IOMode (ReadMode), hClose, hGetContents,
+                                     openFile, putStrLn)
 
 main :: IO ()
-main = loop
-  where
-    loop = do
-      input <- getLine
-      putStrLn . show $ parse input
-      loop
+main = do
+  args <- getArgs
+  if 1 <= length args then do
+    let fileName = head args
+    handle <- openFile fileName ReadMode
+    contents <- hGetContents handle
+    putStrLn . show $ parse contents
+    hClose handle
+  else
+    putStrLn "Argument not specified"
