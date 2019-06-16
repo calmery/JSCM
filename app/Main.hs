58ea5fa0fa2a3c7fb4630d861efbce09b3c18aa4
Marei Kikukawa <contact@calmery.me>
2019-06-17 01:31:12 +0900

    optparse-applicative を使用してコマンドにオプションを追加する

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,45 @@
 module Main where
 
-import           Convert            (convert)
-import           Parser             (parse)
-import           RIO                hiding (hClose)
-import           RIO.List.Partial   (head)
-import           System.Environment (getArgs)
-import           System.IO          (IOMode (ReadMode), hClose, hGetContents,
-                                     openFile, putStrLn)
-import           ToString           (toString)
+import           Convert             (convert)
+import           Options.Applicative as O
+import           Parser              (parse)
+import           RIO                 hiding (hClose)
+import           RIO.List.Partial    (head)
+import           System.Environment  (getArgs)
+import           System.IO           (IOMode (ReadMode), hClose, hGetContents,
+                                      openFile, putStrLn)
+import           ToString            (toString)
+
+data Option = Option {
+  dump  :: Bool,
+  input :: String
+}
 
 main :: IO ()
 main = do
-  args <- getArgs
-  if 1 <= length args then do
-    let fileName = head args
-    handle <- openFile fileName ReadMode
-    contents <- hGetContents handle
-    putStrLn $ case parse fileName contents of
-      Left e       -> show e
-      Right result -> toString $ convert result
-    hClose handle
-  else
-    putStrLn "Argument not specified"
+  options <- O.execParser $ withInfo optionParser
+  handle <- openFile (input options) ReadMode
+  contents <- hGetContents handle
+  putStrLn $ case parse (input options) contents of
+    Left e       -> show e
+    Right result -> if dump options then
+      show result
+    else
+      toString $ convert result
+  hClose handle
+
+optionParser :: O.Parser Option
+optionParser = Option
+  <$> dumpParser
+  <*> inputParser
+  where
+    dumpParser =
+      O.switch ( O.long "dump" <> O.short 'd' <> O.help "Show AST" )
+    inputParser =
+      O.strOption ( O.long "input"  <> metavar "FILE" <> O.short 'i' <> O.help "Input file name" )
+
+withInfo :: O.Parser a -> O.ParserInfo a
+withInfo opts = O.info (O.helper <*> opts) description
+  where
+    description = O.fullDesc
+      <> O.header "jscm - JavaScript compiler for multithreading"
